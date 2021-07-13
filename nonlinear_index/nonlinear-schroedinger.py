92768965c5ce2b8f1b8e90be9b6984c17288f53c
liam-clink <clink.6@osu.edu>
2021-07-12 22:54:48 -0400

    Changed most parameters to real world values
    Electric field amplitude still needs to be changed

--- nonlinear_index/nonlinear-schroedinger.py
+++ nonlinear_index/nonlinear-schroedinger.py
@@ -10,42 +10,44 @@ import matplotlib.pyplot as plt
 # Quoted for 1030nm
 n0 = 1.45
 n2 = 2.2e-16 # cm^2/W
-chi3 = n2*n0**2/(12*np.pi**2) # Formula from Boyd Nonlinear Optics 3ed p213
+n2 /= 100.**2 # m^2/W
+chi3 = n2*n0**2/(12*np.pi**2) # Same units as n2, Formula from Boyd Nonlinear Optics 3ed p213
 c = 299792458. # m/s
 mu_0 = 4e-7*np.pi # H/m
 epsilon_0 = 1./(c**2*mu_0) # F/m
 
-# Returns the polarization field in angular frequency space
-def P(E):
-    # The 3 is because w,w,-w can be distributed in 3 distinct ways
-    # The conjugate is because E_time is real, so E_freq(-w)=E_freq(w)*
-    return 3*epsilon_0*chi3*E*E*np.conjugate(E)
-
 # Returns intensity given the amplitude of the E field wave
 def I(E):
     return .5*n0*epsilon_0*c*E**2
-
+# Returns E wave amplitude from Intensity
+def E(I):
+    return np.sqrt(I/(.5*n0*epsilon_0*c))
 
 ### Solving the nonlinear Schroedinger Equation ###
 
 # Simulation parameters
-thickness = 10.
-duration = 10.
-timestep = 0.01
-spacestep = 0.01
-times = np.arange(0.,duration,timestep)
-spaces = np.arange(0.,thickness,spacestep)
-pulse_duration = 2.
+thickness = 0.5e-3 # m
+duration = 2.5e-12 # s
+times = np.linspace(0.,duration,1000)
+spaces = np.linspace(0.,thickness,1000)
+timestep = times[1]-times[0]
+spacestep = spaces[1]-spaces[0]
+pulse_duration = 260.e-15 # s
 
 # Self phase modulation and dispersion coefficients
-spm_strength = 0.1
+wavelength = 1030.e-9 # m
+angular_frequency = 2*np.pi*c/wavelength
+wavenumber = 2.*np.pi/wavelength
+spm_strength = 3./2.*chi3/(wavenumber*c**2)*angular_frequency**2
+print(spm_strength)
+print(wavenumber)
 dispersion_strength = 0.
 
 # Initialize data container
 A = np.zeros((len(spaces),len(times)),dtype=np.complex128)
 
 # Left boundary condition
-#A_drive = 1.e0 * np.sin(omega * times) * (np.exp(-((times - 0.2) / pulse_duration) ** 2))
+# A_initial = 1.e0 * np.sin(omega * times) * (np.exp(-((times - 0.2) / pulse_duration) ** 2))
 # A is only the envelope
 A_initial = 1.e0 * (np.exp(-((times - duration/2.) / pulse_duration) ** 2))
 
@@ -57,7 +59,7 @@ A[0,:] = A_initial
 # Right hand side of the equation
 def right_hand_side(A):
     # TODO: Currently assuming no dispersion
-    return -1j*A*A*np.conjugate(A)
+    return -1j*spm_strength*A*A*np.conjugate(A)
 
 def RK4(A,dz,f):
     k1 = dz*f(A)
